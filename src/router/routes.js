f6b95b306a68f719858587e3bef93fe8eaa287f0
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-06-25 16:43:36 +0700

    feat: use string compose

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,4 +4,12 @@ export const routes = [
     name: 'index',
     component: () => import('src/pages/index-page.vue'),
   },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('src/pages/auth/auth-login-page.vue'),
+    meta: {
+      layout: 'LayoutAuth',
+    },
+  },
 ];
