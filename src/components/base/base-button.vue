2423ce9e049d9cdc31440dc07c962b241c7e52e6
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-06-26 05:53:18 +0700

    feat: base button loading

--- src/components/base/base-button.vue
+++ src/components/base/base-button.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { computed } from 'vue';
 import { useString } from 'src/composes/resource.compose';
+import BaseSpinner from './base-spinner.vue';
 
 const props = defineProps({
   text: {
@@ -19,6 +20,10 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const { getString } = useString();
@@ -26,18 +31,21 @@ const { getString } = useString();
 const text = computed(() => {
   return props.textFromResource ? getString(props.text) : props.text;
 });
+const isDisabled = computed(() => props.loading);
 const style = computed(() => {
   return {
     base: [
       props.fullwidth ? ' w-full' : '',
-      'flex justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
+      isDisabled.value ? 'opacity-75' : '',
+      'flex justify-center items-center space-x-2 rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
     ],
   };
 });
 </script>
 
 <template>
-  <button :type="props.type" :class="[style.base]">
-    {{ text }}
+  <button :type="props.type" :class="[style.base]" :disabled="isDisabled">
+    <base-spinner v-if="props.loading" size="sm" />
+    <span>{{ text }}</span>
   </button>
 </template>
