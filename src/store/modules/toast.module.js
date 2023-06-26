e6a76580aa7f16cc74b5e64276bda7f9ee970317
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-06-26 19:06:09 +0700

    feat: toast

---
+++ src/store/modules/toast.module.js
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+export const useToastStore = defineStore('toast', () => {
+  const toasts = ref([]);
+  const maxLength = 4;
+
+  async function createToast({
+    id,
+    title,
+    message,
+    type = 'success',
+    autoclose = true,
+  }) {
+    if (toasts.value.length >= maxLength) {
+      toasts.value.shift();
+    }
+
+    toasts.value.push({ id, title, message, type });
+
+    if (autoclose) {
+      setTimeout(() => {
+        removeToast({ index: toasts.value.length - 1 });
+      }, 2000);
+    }
+  }
+
+  function removeToast({ index }) {
+    toasts.value.splice(index, 1);
+  }
+
+  return { toasts, createToast, removeToast };
+});
