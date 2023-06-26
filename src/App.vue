4f2fc326a6fa727de6715c406dae096f54ed3c49
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-06-26 19:07:10 +0700

    feat: app toast

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { useRoute } from 'vue-router';
+import AppToast from 'src/components/app/app-toast.vue';
 
 const route = useRoute();
 </script>
 
 <template>
   <component :is="route.meta.layout">
+    <app-toast />
     <router-view></router-view>
   </component>
 </template>
