f245017a949af93a5c438449814e6c1cf15ea265
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-06-27 07:52:40 +0700

    feat: login

--- src/pages/auth/auth-login-page.vue
+++ src/pages/auth/auth-login-page.vue
@@ -3,6 +3,36 @@ import BaseTitle from 'src/components/base/base-title.vue';
 import BaseInput from 'src/components/base/base-input.vue';
 import BaseButton from 'src/components/base/base-button.vue';
 import BaseLink from 'src/components/base/base-link.vue';
+import { useToastStore } from 'src/store/modules/toast.module';
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+import { useLogin } from 'src/composes/modules/auth/login.compose';
+
+const toastStore = useToastStore();
+const router = useRouter();
+const { login, loading, validation } = useLogin();
+
+const form = reactive({
+  email: null,
+  password: null,
+});
+
+async function handleSubmit() {
+  const [success, error] = await login(form);
+
+  if (!success) {
+    if (error) {
+      toastStore.createToast({
+        id: 'error-auth',
+        title: error,
+        message: error,
+        type: 'error',
+      });
+    }
+  } else {
+    router.push({ name: 'index' });
+  }
+}
 </script>
 
 <template>
@@ -17,30 +47,37 @@ import BaseLink from 'src/components/base/base-link.vue';
     </div>
 
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <form class="space-y-6" action="#" method="POST">
+      <form class="space-y-6" v-on:submit.prevent="handleSubmit">
         <base-input
           id="email"
           type="email"
           placeholder="auth.login.form.email"
           label="auth.login.form.email"
+          :color="validation.email ? 'red' : 'gray'"
+          :message="validation.email"
           with-label
           label-from-resource
           placeholder-from-resource
+          v-model="form.email"
         />
         <base-input
           id="password"
           type="password"
           placeholder="auth.login.form.password"
           label="auth.login.form.password"
+          :color="validation.password ? 'red' : 'gray'"
+          :message="validation.password"
           with-label
           label-from-resource
           placeholder-from-resource
+          v-model="form.password"
         />
         <base-button
           type="submit"
           text="auth.login.form.submit"
           text-from-resource
           fullwidth
+          :loading="loading"
         />
       </form>
     </div>
---
+++ src/store/modules/auth.module.js
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+export const useAuthStore = defineStore('auth', () => {
+  const token = ref(null);
+  const me = ref(null);
+  const isLoggedIn = ref(false);
+
+  async function login(data) {
+    token.value = data.token;
+    me.value = data.me;
+    isLoggedIn.value = true;
+  }
+
+  return { login };
+});
