05a57b6d5556a00e1135aa0c3fb25446b8c0253a
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-06-29 19:20:33 +0700

    feat: layout app

--- src/pages/index-page.vue
+++ src/pages/index-page.vue
@@ -1,3 +1,14 @@
+<script setup>
+import BaseHeader from 'src/components/base/base-header.vue';
+</script>
+
 <template>
-  <div>Index</div>
+  <div>
+    <base-header title="pages.index" title-from-resource></base-header>
+    <main>
+      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <!-- Your content -->
+      </div>
+    </main>
+  </div>
 </template>
