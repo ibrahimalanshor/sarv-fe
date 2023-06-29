52a097c23cc46abc9b2851a373d774b190e5816d
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-06-29 11:19:12 +0700

    feat: app progress bar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { useRoute } from 'vue-router';
 import AppToast from 'src/components/app/app-toast.vue';
+import AppProgressBar from 'src/components/app/app-progress-bar.vue';
 
 const route = useRoute();
 </script>
 
 <template>
   <component :is="route.meta.layout">
+    <app-progress-bar />
     <app-toast />
     <router-view></router-view>
   </component>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import App from './App.vue';
 import { setupStore } from './store/setup';
 import { setupRouter } from './router/setup';
 import { useDynamicLayout } from './plugins/dynamic-layout';
+import { useProgressBar } from './plugins/progress-bar';
 
 const app = createApp(App);
 
 useDynamicLayout(app);
+useProgressBar(app);
 
 setupStore(app);
 setupRouter(app);
