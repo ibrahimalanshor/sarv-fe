fdadc9e566a5e3fdb1df1ae6a9c1d061e6f26c15
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-06-29 21:17:41 +0700

    feat: http authorization

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,7 +1,34 @@
 import axios from 'axios';
+import { router } from 'src/router/router';
+import { useAuthStore } from 'src/store/modules/auth.module';
 
 const http = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+http.interceptors.request.use((config) => {
+  const authStore = useAuthStore();
+
+  if (authStore.isLoggedIn) {
+    config.headers.Authorization = `Bearer ${authStore.token}`;
+  }
+
+  return config;
+});
+
+http.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    const authStore = useAuthStore();
+
+    if (error.response.status === 401) {
+      authStore.logout();
+
+      router.push({ name: 'login' });
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export { http };
