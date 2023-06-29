d45a5957725309c262772aaeef179c092d1134d3
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-06-29 19:18:15 +0700

    feat: header

---
+++ src/components/base/base-header.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { computed } from 'vue';
+import { useString } from 'src/composes/resource.compose';
+import BaseTitle from './base-title.vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  titleFromResource: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const { getString } = useString();
+
+const title = computed(() => {
+  return props.titleFromResource ? getString(props.title) : props.title;
+});
+</script>
+
+<template>
+  <header class="bg-white shadow">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <base-title :level="3">{{ title }}</base-title>
+    </div>
+  </header>
+</template>
