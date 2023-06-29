74ba9a02d138bff8f7dc9b1fd95679ac5aff2007
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-06-29 19:19:24 +0700

    feat: clikc outside

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import { setupStore } from './store/setup';
 import { setupRouter } from './router/setup';
 import { useDynamicLayout } from './plugins/dynamic-layout';
 import { useProgressBar } from './plugins/progress-bar';
+import { useClickOutside } from './plugins/click-outside';
 
 const app = createApp(App);
 
 useDynamicLayout(app);
 useProgressBar(app);
+useClickOutside(app);
 
 setupStore(app);
 setupRouter(app);
