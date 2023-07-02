cda1cfd1577c0c730163cc969a0f6efaadfe0ec9
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-07-02 13:32:33 +0700

    feat: task table

--- src/pages/task/task-index-page.vue
+++ src/pages/task/task-index-page.vue
@@ -1,29 +1,50 @@
 <script setup>
 import { useRequest } from 'src/composes/request.compose';
+import { useString } from 'src/composes/resource.compose';
 import BaseHeader from 'src/components/base/base-header.vue';
 import BaseContainer from 'src/components/base/base-container.vue';
 import BaseTable from 'src/components/base/base-table.vue';
+import TaskStatusDropdown from 'src/components/modules/task-status/task-status-dropdown.vue';
+import { h, reactive } from 'vue';
 
-const { error, loading, request } = useRequest({
+const { getString } = useString();
+const { data, loading, request } = useRequest({
   method: 'get',
   url: '/api/tasks',
   notifyOnError: true,
   initLoading: true,
+  initData: { data: [] },
 });
 
+const fetchTasksParams = reactive({
+  include: ['category', 'status'],
+});
 const tableColumns = [
   {
     id: 'name',
-    name: 'Name',
+    name: getString('task.columns.name'),
+    bold: true,
   },
   {
     id: 'category',
-    name: 'Category',
+    name: getString('task.columns.category'),
+    value: (item) => item.category.name,
+  },
+  {
+    id: 'status',
+    name: getString('task.columns.status'),
+    value: (item) => (item.status ? item.status.name : 'No Status'),
+    render: ({ item }) =>
+      h(TaskStatusDropdown, {
+        task: item,
+        modelValue: item.status,
+        'onUpdate:modelValue': (value) => (item.status = value),
+      }),
   },
 ];
 
 async function fetchTasks() {
-  await request();
+  await request({ params: fetchTasksParams });
 }
 
 fetchTasks();
@@ -34,7 +55,11 @@ fetchTasks();
     <base-header title="pages.task.index" title-from-resource></base-header>
     <main>
       <base-container>
-        <base-table :columns="tableColumns" :data="[]" :loading="loading" />
+        <base-table
+          :columns="tableColumns"
+          :data="data.data"
+          :loading="loading"
+        />
       </base-container>
     </main>
   </div>
