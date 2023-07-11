b53911ab276b56fb67487b0524bd9788da2cd306
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-07-12 06:31:37 +0700

    feat: task status quick action on table

--- src/components/modules/task-category/task-category-detail-modal.vue
+++ src/components/modules/task-category/task-category-detail-modal.vue
@@ -7,8 +7,6 @@ import BaseButton from 'src/components/base/base-button.vue';
 import BaseSkeleton from 'src/components/base/base-skeleton.vue';
 import BaseDescription from 'src/components/base/base-description.vue';
 import { computed, ref } from 'vue';
-import TaskCategoryEditModal from './task-category-edit-modal.vue';
-import TaskCategoryDeleteConfirm from './task-category-delete-confirm.vue';
 
 const props = defineProps({
   modelValue: {
@@ -41,9 +39,6 @@ const {
   initData: {},
 });
 
-const editModalVisible = ref(false);
-const deleteConfirmVisible = ref(false);
-
 const visible = computed({
   get() {
     return props.modelValue;
@@ -75,22 +70,6 @@ function handleVisible() {
 function handleCloseAlert() {
   resetGetTaskCategoryError();
 }
-function handleEdit() {
-  editModalVisible.value = true;
-}
-function handleDelete() {
-  deleteConfirmVisible.value = true;
-}
-function handleUpdated() {
-  loadTaskCategory();
-
-  emit('updated');
-}
-function handleDeleted() {
-  visible.value = false;
-
-  emit('deleted');
-}
 </script>
 
 <template>
@@ -112,35 +91,11 @@ function handleDeleted() {
           v-on:close="handleCloseAlert"
         />
         <base-description :attributes="attributes" :data="taskCategory" />
-        <task-category-edit-modal
-          :task-category="taskCategory"
-          v-model="editModalVisible"
-          v-on:updated="handleUpdated"
-        />
-        <task-category-delete-confirm
-          :task-category="taskCategory"
-          v-model="deleteConfirmVisible"
-          v-on:deleted="handleDeleted"
-        />
       </template>
     </div>
 
     <template #footer="{ close }">
-      <div class="flex items-center justify-between">
-        <div class="flex items-center gap-x-2">
-          <base-button
-            text="actions.edit"
-            color="indigo"
-            text-from-resource
-            v-on:click="handleEdit"
-          />
-          <base-button
-            text="actions.delete"
-            color="red"
-            text-from-resource
-            v-on:click="handleDelete"
-          />
-        </div>
+      <div class="flex items-center justify-end">
         <base-button
           text="actions.cancel"
           text-from-resource
--- src/pages/task-category/task-category-index-page.vue
+++ src/pages/task-category/task-category-index-page.vue
@@ -8,6 +8,8 @@ import BaseInput from 'src/components/base/base-input.vue';
 import BaseButton from 'src/components/base/base-button.vue';
 import TaskCategoryCreateModal from 'src/components/modules/task-category/task-category-create-modal.vue';
 import TaskCategoryDetailModal from 'src/components/modules/task-category/task-category-detail-modal.vue';
+import TaskCategoryEditModal from 'src/components/modules/task-category/task-category-edit-modal.vue';
+import TaskCategoryDeleteConfirm from 'src/components/modules/task-category/task-category-delete-confirm.vue';
 import { h, reactive, ref } from 'vue';
 
 const { getString } = useString();
@@ -34,6 +36,14 @@ const detailModal = reactive({
   visible: false,
   taskCategoryId: null,
 });
+const editModal = reactive({
+  taskCategory: {},
+  visible: false,
+});
+const deleteConfirm = reactive({
+  taskCategory: {},
+  visible: false,
+});
 
 const tableColumns = [
   {
@@ -49,6 +59,33 @@ const tableColumns = [
           onClick: () => handleDetail(item),
         },
         item.name
+      ),
+  },
+  {
+    id: 'action',
+    sortable: false,
+    render: ({ item }) =>
+      h(
+        'div',
+        { class: 'flex items-center justify-end gap-x-2' },
+        {
+          default: () => [
+            h(BaseButton, {
+              size: 'sm',
+              text: 'actions.edit',
+              textFromResource: true,
+              color: 'green',
+              onClick: () => handleEdit(item),
+            }),
+            h(BaseButton, {
+              size: 'sm',
+              text: 'actions.delete',
+              textFromResource: true,
+              color: 'red',
+              onClick: () => handleDelete(item),
+            }),
+          ],
+        }
       ),
   },
 ];
@@ -93,6 +130,14 @@ function handleRefresh() {
 function handleDetail(item) {
   detailModal.taskCategoryId = item.id;
   detailModal.visible = true;
+}
+function handleEdit(item) {
+  editModal.taskCategory = item;
+  editModal.visible = true;
+}
+function handleDelete(item) {
+  deleteConfirm.taskCategory = item;
+  deleteConfirm.visible = true;
 }
 
 loadTaskCategories();
@@ -146,7 +191,15 @@ loadTaskCategories();
     <task-category-detail-modal
       :task-category-id="detailModal.taskCategoryId"
       v-model="detailModal.visible"
+    />
+    <task-category-edit-modal
+      :task-category="editModal.taskCategory"
+      v-model="editModal.visible"
       v-on:updated="handleRefresh"
+    />
+    <task-category-delete-confirm
+      :task-category="deleteConfirm.taskCategory"
+      v-model="deleteConfirm.visible"
       v-on:deleted="handleRefresh"
     />
   </div>
