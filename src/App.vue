81bec448d49521a266cb8ecda3fdfe880b30c096
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-07-19 18:42:58 +0700

    feat: me info navbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <script setup>
 import { useRoute, useRouter } from 'vue-router';
 import { useTitle } from 'src/composes/title.compose';
+import { useMe } from 'src/composes/modules/auth/me.compose';
 import AppToast from 'src/components/app/app-toast.vue';
 import AppProgressBar from 'src/components/app/app-progress-bar.vue';
 
 const route = useRoute();
 const router = useRouter();
 const { setTitleFromResource } = useTitle();
+const { loadMe } = useMe();
 
 router.afterEach(() => {
   setTitleFromResource();
 });
+
+loadMe();
 </script>
 
 <template>
---
+++ src/composes/modules/auth/me.compose.js
@@ -0,0 +1,20 @@
+import { useRequest } from 'src/composes/request.compose';
+import { useAuthStore } from 'src/store/modules/auth.module';
+
+export function useMe() {
+  const authStore = useAuthStore();
+  const { request: fetchMe } = useRequest({
+    method: 'get',
+    url: '/api/me',
+  });
+
+  async function loadMe() {
+    const [res] = await fetchMe();
+
+    if (res) {
+      authStore.setMe(res);
+    }
+  }
+
+  return { loadMe };
+}
