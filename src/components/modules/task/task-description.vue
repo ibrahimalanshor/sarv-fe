0ff7fc9224fa14818ec46a19c326b521bd49945d
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-07-29 14:17:05 +0700

    feat: fixing task description

--- src/components/modules/task/task-description.vue
+++ src/components/modules/task/task-description.vue
@@ -8,17 +8,19 @@ import { useString } from 'src/composes/resource.compose';
 import { formatDate, toDate } from 'src/utils/date';
 import TaskChildrenStatus from './task-children-status.vue';
 import { hasChildren } from 'src/helpers/modules/task.helper';
+import { conditionalElement } from 'src/utils/array.js';
+import { nullable } from 'src/utils/type.js';
 
 const props = defineProps({
   task: {
     type: Object,
     required: true,
   },
-  inline: {
-    type: Boolean,
-    default: false,
-  },
   attributes: {
+    type: Object,
+    default: () => ({}),
+  },
+  baseDescriptionProps: {
     type: Object,
     default: () => ({}),
   },
@@ -29,77 +31,61 @@ const { getString } = useString();
 
 const attributes = computed(() => {
   return [
-    {
+    ...conditionalElement(nullable(props.attributes.name, true), {
       id: 'name',
       name: getString('task.attributes.name'),
-    },
-    ...(props.attributes.category ?? true
-      ? [
-          {
-            id: 'category',
-            name: getString('task.attributes.category'),
-            render: () =>
-              props.task.category
-                ? h(
-                    RouterLink,
-                    {
-                      class: 'hover:underline',
-                      to: {
-                        name: 'task-category.detail',
-                        params: { id: props.task.category.id },
-                      },
-                    },
-                    { default: () => props.task.category.name }
-                  )
-                : h('span', {}, '-'),
-          },
-        ]
-      : []),
-    {
+    }),
+    ...conditionalElement(nullable(props.attributes.category, true), {
+      id: 'category',
+      name: getString('task.attributes.category'),
+      render: () =>
+        props.task.category
+          ? h(
+              RouterLink,
+              {
+                class: 'hover:underline',
+                to: {
+                  name: 'task-category.detail',
+                  params: { id: props.task.category.id },
+                },
+              },
+              { default: () => props.task.category.name }
+            )
+          : h('span', {}, '-'),
+    }),
+    ...conditionalElement(nullable(props.attributes.created_at, true), {
       id: 'created_at',
       name: getString('task.attributes.created_at'),
       value: formatDate(props.task.created_at),
-    },
-    ...(props.attributes.due_date ?? true
-      ? [
-          {
-            id: 'due_date',
-            name: getString('task.attributes.due_date'),
-            value: props.task.due_date ? toDate(props.task.due_date) : '-',
-          },
-        ]
-      : []),
-    {
+    }),
+    ...conditionalElement(nullable(props.attributes.due_date, true), {
+      id: 'due_date',
+      name: getString('task.attributes.due_date'),
+      value: props.task.due_date ? toDate(props.task.due_date) : '-',
+    }),
+    ...conditionalElement(nullable(props.attributes.status, true), {
       id: 'status',
       name: getString('task.attributes.status'),
-    },
-    ...(hasChildren(props.task) && (props.attributes.children_count ?? true)
-      ? [
-          {
-            id: 'children_count',
-            name: getString('task.attributes.sub_task_status'),
-            render: () => h(TaskChildrenStatus, { meta: props.task.meta }),
-          },
-        ]
-      : []),
-    ...(props.attributes.priority ?? true
-      ? [
-          {
-            id: 'priority',
-            name: getString('task.attributes.priority'),
-          },
-        ]
-      : []),
-    ...(props.attributes.description ?? true
-      ? [
-          {
-            id: 'description',
-            name: getString('task.attributes.description'),
-            value: props.task.description ?? '-',
-            fullwidth: props.task.meta.children_count === 0,
-          },
-        ]
-      : []),
+    }),
+    ...conditionalElement(
+      hasChildren(props.task) &&
+        nullable(props.attributes.children_count, true),
+      {
+        id: 'children_count',
+        name: getString('task.attributes.sub_task_status'),
+        render: () => h(TaskChildrenStatus, { meta: props.task.meta }),
+      }
+    ),
+    ...conditionalElement(nullable(props.attributes.priority, true), {
+      id: 'priority',
+      name: getString('task.attributes.priority'),
+    }),
+    ...conditionalElement(nullable(props.attributes.description, true), {
+      id: 'description',
+      name: getString('task.attributes.description'),
+      value: props.task.description ?? '-',
+      fullwidth: props.task.meta.children_count === 0,
+    }),
   ];
 });
 
@@ -110,9 +96,9 @@ function hanldeUpdatedStatus() {
 
 <template>
   <base-description
-    :inline="props.inline"
     :attributes="attributes"
     :data="task"
+    v-bind="props.baseDescriptionProps"
   >
     <template #[`priority`]>
       <task-priority-badge v-if="task.priority" :task="task" />
