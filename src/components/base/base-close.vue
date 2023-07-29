f3b1bf70a05e309d66ff96a87201560d07aec4a9
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-07-29 19:53:55 +0700

    feat: action button

--- src/components/base/base-close.vue
+++ src/components/base/base-close.vue
@@ -1,4 +1,6 @@
 <script setup>
+import BaseActionButton from './base-action-button.vue';
+
 const emit = defineEmits(['close']);
 
 function handleClick() {
@@ -7,12 +9,7 @@ function handleClick() {
 </script>
 
 <template>
-  <button
-    type="button"
-    class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-    v-on:click="handleClick"
-  >
-    <span class="sr-only">Close</span>
+  <base-action-button v-on:click="handleClick">
     <svg
       class="h-5 w-5"
       viewBox="0 0 20 20"
@@ -23,5 +20,5 @@ function handleClick() {
         d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"
       />
     </svg>
-  </button>
+  </base-action-button>
 </template>
